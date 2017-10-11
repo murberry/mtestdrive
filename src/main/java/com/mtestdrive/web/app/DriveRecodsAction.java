018267295e39d4422ca847f0ffcad360fa23fbfd
tim-yin <yinwei1985_ren@163.com>
2017-10-11 18:37:41 +0800

    打印调试 车辆 信息 打日志信息

--- src/main/java/com/mtestdrive/web/app/DriveRecodsAction.java
+++ src/main/java/com/mtestdrive/web/app/DriveRecodsAction.java
@@ -346,6 +346,7 @@ public class DriveRecodsAction extends BaseController {
 		String customerId = request.getParameter("customerId");
 		DriveRecodsVo driveRecodsVo = null;
 		if (StringUtil.isNotEmpty(id)) {
+			logger.info("进入分支1");
 			DriveRecodsEntity driveRecods = driveRecodsService.get(DriveRecodsEntity.class, id);
 			driveRecodsVo = new DriveRecodsVo();
 			try {
@@ -361,6 +362,7 @@ public class DriveRecodsAction extends BaseController {
 				e.printStackTrace();
 			}
 		} else if (StringUtil.isNotEmpty(customerId)) {
+			logger.info("进入分支2");
 			CustomerInfoEntity customer = customerInfoService.get(CustomerInfoEntity.class, customerId);
 			driveRecodsVo = new DriveRecodsVo();
 			driveRecodsVo.setCustomer(customer);
@@ -371,6 +373,9 @@ public class DriveRecodsAction extends BaseController {
 		DetachedCriteria dc = DetachedCriteria.forClass(CarInfoEntity.class);
 		dc.add(Restrictions.eq("agency.id", ((CustomerInfoEntity)driveRecodsVo.getCustomer()).getAgencyId()));
 		dc.add(Restrictions.ne("status", CarStatus.NO_USED));
+
+		logger.info("agency_id:" +((CustomerInfoEntity)driveRecodsVo.getCustomer()).getAgencyId());
+		logger.info("status:" + CarStatus.NO_USED);
 				
 		List<CarInfoEntity> cars = carInfoService.findByDetached(dc);
 		logger.info("查询车辆信息集合size:" + cars.size());
